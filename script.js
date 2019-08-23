8801918e46de7d22abbb096c8b188d796426f9ef
qusai <qusai@wso2.com>
2019-08-24 01:33:22 +0530

    after putting buttons

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 const firebaseRef = firebase.database();
 let tokenNumber = 0;
+let counters = {};
+let counterDetails = [];
+let tokenData = {};
+let tokenProperties = [];
 
-document.querySelector("button.submitButton").addEventListener("click", () => {
-  firebaseRef.ref().once('value', function(snapshot) {
-    if (snapshot.toJSON().tokens) {
-      tokenData = snapshot.toJSON().tokens;
-      tokenProperties = Object.keys(tokenData);
-      for (var i = 0; i < tokenProperties.length  ; i++) {
-        let existingToken = Number(tokenProperties[i]);
-        if (existingToken > tokenNumber) {
-          tokenNumber = existingToken;
-        }
-      }
+firebaseRef.ref().once("value", function (snapshot) {
+    counters = snapshot.toJSON().counters;
+    counterDetails = Object.keys(counters);
+    let displayIssueButtons = "";
+    for (let i = 0; i < counterDetails.length; i++) {
+        displayIssueButtons += "<button type='button' class='btn btn-primary btn-lg btn-block my-btn' value='" + counters[counterDetails[i]].name + "' onclick='getCounterName(event)'>" + counters[counterDetails[i]].name + "</button>"
     }
-    const selectRef = document.querySelector("select.issues");
-    const issueSubmitted = selectRef.options[selectRef.selectedIndex].text;
-    tokenNumber++;
-    if (tokenNumber >= 0 && tokenNumber <= 999) {
-      tokenNumber = ("000" + tokenNumber).substr(-3);
-    }
-
-    firebaseRef.ref("tokens/" + tokenNumber).set({
-      token: tokenNumber,
-      issue: issueSubmitted,
-      pending: true
-    });
-    console.log("Data submitted: \nToken Number: " + tokenNumber + "\nIssue: " + issueSubmitted);
-    document.getElementsByClassName("submitText")[0].style.display = "block";
-    setTimeout(() => {
-      document.getElementsByClassName("submitText")[0].style.display = "none";
-    }, 6000);
-  });
+    document.querySelector("div.issue-buttons").innerHTML = displayIssueButtons;
 });
+
+function getCounterName(event) {
+    firebaseRef.ref().once("value", function (snapshot) {
+        counters = snapshot.toJSON().counters;
+        counterDetails = Object.keys(counters);
+
+        for (let i = 0; i < counterDetails.length; i++) {
+            if (counters[counterDetails[i]].name === event.target.getAttribute("value")) {
+                if (snapshot.toJSON().tokens) {
+                    tokenData = snapshot.toJSON().tokens;
+                    tokenProperties = Object.keys(tokenData);
+                    for (var j = 0; j < tokenProperties.length; j++) {
+                        let existingToken = Number(tokenProperties[j]);
+                        if (existingToken > tokenNumber) {
+                            tokenNumber = existingToken;
+                        }
+                    }
+                }
+                console.log(tokenNumber);
+                tokenNumber++;
+                if (tokenNumber >= 0 && tokenNumber <= 999) {
+                    tokenNumber = ("000" + tokenNumber).substr(-3);
+                }
+
+                firebaseRef.ref("tokens/" + tokenNumber).set({
+                    token: tokenNumber,
+                    prefix: counters[counterDetails[i]].prefix,
+                    issue: counters[counterDetails[i]].name,
+                    pending: true
+                });
+                console.log("Data submitted: \nToken Number: " + tokenNumber + "\nIssue: " + counters[counterDetails[i]].name);
+            }
+          document.getElementsByClassName("submitText")[0].style.display = "block";
+        }
+        setTimeout(() => {
+            document.getElementsByClassName("submitText")[0].style.display = "none";
+        }, 6000);
+    });
+}
+
+
+
